c025e0dae48a7b041cd5f4aae0e85639fbc38f9c
Rutuja-Rajendra <rutub175@gmail.com>
2025-12-01 23:13:04 +0530

    Daily practice

--- String/Anagram.java
+++ String/Anagram.java
@@ -2,6 +2,31 @@ package String;
 
 public class Anagram {
     public static void main(String[] args) {
+
+        String s1 = "listen";
+        String s2 = "silent";
+        System.out.println(areAnagrams(s1, s2));
+    }
+
+        public static boolean areAnagrams(String str1, String str2) {
+            if (str1.length() != str2.length()) {
+                return false;
+            }
+
+            int[] charCount = new int[256]; // Assuming ASCII character set
+
+            for (int i = 0; i < str1.length(); i++) {
+                charCount[str1.charAt(i)]++;
+                charCount[str2.charAt(i)]--;
+            }
+
+            for (int count : charCount) {
+                if (count != 0) {
+                    return false;
+                }
+            }
+
+            return true;
         
     }
 }
