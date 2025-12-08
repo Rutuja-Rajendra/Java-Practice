c9daa6c33820033b0dd1dc3aa63915873c10fd16
Rutuja-Rajendra <rutub175@gmail.com>
2025-12-09 00:01:29 +0530

    Daily practice

---
+++ String/SubstringFreq.java
@@ -0,0 +1,19 @@
+package String;
+
+public class SubstringFreq {
+    public static void main(String[] args) {
+        
+        //Find how many times a substring appears in a string.
+
+        String word = "popcornpop";
+
+        String substring = "pop";
+
+        int freq = 0;
+
+        for(int i = 0; i < word.length(); i++)
+        {
+            
+        }
+    }
+}
