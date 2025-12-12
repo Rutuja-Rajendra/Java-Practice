e27082e489015c79b48ff05e13840eb9dfa40558
Rutuja-Rajendra <rutub175@gmail.com>
2025-12-12 21:18:16 +0530

    Daily practice

---
+++ String/Declaration.java
@@ -0,0 +1,13 @@
+package String;
+
+public class Declaration
+{
+    public static void main(String[] args) {
+        
+        String s = "Hello";
+
+        String s1 = new String("World");
+
+        System.out.println(s +" "+s1);
+    }
+}
