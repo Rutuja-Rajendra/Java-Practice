86af0a6e48882346d4cd4f3a2464fb017be3abec
Rutuja-Rajendra <rutub175@gmail.com>
2025-12-12 21:25:55 +0530

    Daily  practice

--- String/Declaration.java
+++ String/Declaration.java
@@ -4,10 +4,10 @@ public class Declaration
 {
     public static void main(String[] args) {
         
-        String s = "Hello";
+        String s1 = "Hello";
 
-        String s1 = new String("World");
+        String s2 = new String("World");
 
-        System.out.println(s +" "+s1);
+        System.out.println(s1 +" "+s2);
     }
 }
